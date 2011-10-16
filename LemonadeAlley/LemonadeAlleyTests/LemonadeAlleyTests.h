71e04dd9c03e67b416ea885f8828ef3ad626b223
James Wang <jianshi.wang@gmail.com>
2011-10-16 13:33:57 -1000

    iOS 5 storyboard and taking out three20.

---
+++ LemonadeAlley/LemonadeAlleyTests/LemonadeAlleyTests.h
@@ -0,0 +1,13 @@
+//
+//  LemonadeAlleyTests.h
+//  LemonadeAlleyTests
+//
+//  Created by Jian Shi Wang on 10/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LemonadeAlleyTests : SenTestCase
+
+@end
---
+++ LemonadeAlley/LemonadeAlleyTests/LemonadeAlleyTests.m
@@ -0,0 +1,32 @@
+//
+//  LemonadeAlleyTests.m
+//  LemonadeAlleyTests
+//
+//  Created by Jian Shi Wang on 10/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LemonadeAlleyTests.h"
+
+@implementation LemonadeAlleyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LemonadeAlleyTests");
+}
+
+@end
